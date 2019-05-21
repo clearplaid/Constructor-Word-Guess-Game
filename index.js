80cad72dd427f7f71987b2d77f6f2b209674a1f5
Scott <clearplaid@gmail.com>
2019-05-21 12:28:31 -0400

    created initial files, gitignore, pkg json, and added notes to what needs to be done on each js file

---
+++ index.js
@@ -0,0 +1,11 @@
+var inquirer = require('inquirer');
+var Word = require('./word');
+
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then(answers => {
+    // Use user feedback for... whatever!!
+  });
